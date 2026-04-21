b06b70d23623b652575dee1ba0558f35642ba133
Parikshit Gautam <parikshit10gautam@gmail.com>
2026-04-21 16:46:18 +0545

    updated vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['react-is'],
+  },
   server: {
     proxy: {
       '/api': {
@@ -17,7 +20,7 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('node_modules/react') || id.includes('node_modules/react-dom') || id.includes('node_modules/react-router-dom')) {
+          if (id.includes('node_modules/react') || id.includes('node_modules/react-dom') || id.includes('node_modules/react-router-dom') || id.includes('node_modules/react-is')) {
             return 'vendor';
           }
           if (id.includes('node_modules/recharts')) {
